1e2ffbf7249587fea14994ac8e93244a7a750389
Caglar Mehmet <mcm.str.bitcoin@gmail.com>
2018-08-27 17:14:02 +0200

    main sol file updated

--- CryptoColors.sol
+++ CryptoColors.sol
@@ -15,7 +15,7 @@ contract CryptoColors {
     address public owner;
     
     GameTable[] public tables; 
-
+    mapping (address => Player) public addressToPlayer;
 
     struct GameTable {
         uint32 tableId;
@@ -26,6 +26,71 @@ contract CryptoColors {
         uint32[] colorGrid;
         address[] players;
     }
+    
+     struct Player {
+        address playerAddress;
+        string name;
+        uint32 colorId;
+        uint32 tableId;
+        uint32 betMultiplier;
+        uint32 numberOfColors; 
+    }
+    
+    modifier onlyOwner {
+         require (msg.sender == owner);
+         _;
+    }
+    
+    modifier tableNotExists(uint32 tableId) {
+        bool found = false;
+        for (uint32 i = 0; i < tables.length; i++) {
+            if (tables[i].tableId == tableId) {
+                found = true;
+                break;
+            }
+        }
+        require(found == false);
+        _;
+    }
+    
+    modifier tableExists(uint32 tableId) {
+        bool found = false;
+        for (uint32 i = 0; i < tables.length; i++) {
+            if (tables[i].tableId == tableId) {
+                found = true;
+                break;
+            }
+        }
+        require(found == true);
+        _;
+    }
+    
+    modifier playerExists(address playerAddress) {
+        require(addressToPlayer[playerAddress].playerAddress != 0);
+        _;
+    }
+    
+
+    modifier isValidBet(uint betValue) {
+        require(betValue >= minBetMultiplier && betValue <= maxBetMultiplier);
+        _;
+    }
+     
+    
+    event PlayerJoinedForAll(address playerAddress); 
+    event PlayerJoined(address indexed playerAddress);
+    event PlayerInTableForAll(address playerAddress, uint32 tableId); 
+    event PlayerInTable(address indexed playerAddress, uint32 tableId);
+    
+    event NoTableFound(address indexed playerAddress);
+    
+    event GameIsStartedAtTable(uint32 tableId);
+    event GameIsEndedAtTable(uint32 tableId);
+
+    event PlayerWon(address indexed playerAddress);
+    event PlayerLost(address indexed playerAddress);
+    event PlayerIsPaid(address indexed playerAddress);
+    
     
     // Constructor
     constructor() public {
@@ -40,7 +105,8 @@ contract CryptoColors {
     }
     
     
-    function initGamePlay() external {
+    // Init all the tables. This should be called after the contract is constructed
+    function initGamePlay() external onlyOwner {
         // tableId = 0 is reserved.
         for (uint32 i = 1; i <= maxTableNumber; i++) {
             address[] memory tempPlayerAddresses = new address[](maxNumOfPeopleAtTable);
@@ -51,8 +117,35 @@ contract CryptoColors {
     }
     
     
+    // Get the game variables, onlyOwner can get it  
+    function getConstantGameVariables() external view onlyOwner returns(uint32, uint32, uint32, uint256, uint32)  {
+        return (minBetMultiplier, maxBetMultiplier, maxTableNumber, betConstantFinney, maxWaitingTimeForPlayers);
+    }
+    
+    // Change the game vars. Only admin (owner) can change
+    function setGameVars(uint32 argMinBetMultiplier, uint32 argMaxBetMultiplier, uint32 argMaxTableNumber, uint256 argBetConstantFinney, 
+                         uint32 argMaxWaitingTimeForPlayers) external onlyOwner {
+                             
+        minBetMultiplier = argMinBetMultiplier;
+        maxBetMultiplier = argMaxBetMultiplier;
+        maxTableNumber = argMaxTableNumber;
+        betConstantFinney = argBetConstantFinney;
+        maxWaitingTimeForPlayers = argMaxWaitingTimeForPlayers;
+    }
+    
+    
+    // Admin can increase the number of tables 
+    function addNewTable(uint32 tableId) external onlyOwner tableNotExists(tableId) {
+        address[] memory tempPlayerAddresses = new address[](maxNumOfPeopleAtTable);
+        uint32[] memory tempColorGrid = new uint32[](maxColorsAtTable);
+        GameTable memory tempTable = GameTable(tableId, 0, 0, 0, 0, tempColorGrid, tempPlayerAddresses); 
+        tables.push(tempTable);
+    }
+    
+    // Return all the info for a given table
     function getTableInfo(uint32 tableId) external view returns(uint32, uint32, uint32, uint32, uint32[], address[]) {
         GameTable storage table = tables[tableId];
         return (table.playerCount, table.firstPlayerArrivedTime, table.totalColorNumber, table.winnerNumber, table.colorGrid, table.players);
     }
+    
 }
